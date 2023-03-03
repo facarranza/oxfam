f66169541a4984c573ec0227f2f3475cb6d1a25f
CamilaAchuri <camilaachury.4@gmail.com>
2023-03-03 20:16:48 -0300

    prep oxfam data

---
+++ data-raw/DATADRIVE.R
@@ -0,0 +1,46 @@
+library(googlesheets4)
+library(dplyr)
+gs4_deauth()
+general_info <- read_sheet("https://docs.google.com/spreadsheets/d/1tjMuZuPliEdssJjqZtTKsOC8x5WR3ENwlWoCp-Dhhvk/edit#gid=0", "slug_descripcion")
+
+slug_translate <- general_info |>
+  select(slug, slug_es = nombre_es, slug_en = name, slug_pt = nome)
+usethis::use_data(slug_translate, overwrite = TRUE)
+
+
+slug_extras <-  general_info |>
+  select(slug, slug_description_es = descripcion_es,
+         slug_description_en = description, fuente, url)
+usethis::use_data(slug_extras, overwrite = TRUE)
+
+
+slug_filters <- general_info |>
+  select(slug, filtro_fecha, filtro_pais, filtro_unidad)
+usethis::use_data(slug_filters, overwrite = TRUE)
+
+
+slug_viz <- read_sheet("https://docs.google.com/spreadsheets/d/1tjMuZuPliEdssJjqZtTKsOC8x5WR3ENwlWoCp-Dhhvk/edit#gid=0", "matriz_visualizaciones")
+slug_agg <- slug_viz |>
+  select(slug, agg = mapa) |>
+  filter(agg %in% c("mean", "sum,mean", "sum"))
+slug_agg$agg <- as.character(slug_agg$agg)
+usethis::use_data(slug_agg, overwrite = TRUE)
+
+
+slug_viz <- slug_viz |>
+  select(slug, map = mapa,
+         bar = barras,
+         line = linea,
+         treemap,
+         scatter = scatter_plot,
+         sankey) |>
+  gather("viz", "agg", -slug) |>
+  filter(agg != "0") |> select(-agg)
+usethis::use_data(slug_viz, overwrite = TRUE)
+
+
+slug_comparisons <- read_sheet("https://docs.google.com/spreadsheets/d/1tjMuZuPliEdssJjqZtTKsOC8x5WR3ENwlWoCp-Dhhvk/edit#gid=0", "matriz_comparaciones")
+slug_comparisons <- slug_comparisons |>
+  select(indicador, slug = indicador_comparacion, notas)
+usethis::use_data(slug_comparisons, overwrite = TRUE)
+
---
+++ data-raw/DATASET.R
@@ -0,0 +1,84 @@
+## code to prepare `DATASET` dataset goes here
+library(DBI)
+library(purrr)
+library(tidyr)
+library(googlesheets4)
+library(oxfam)
+
+readRenviron(".Renviron")
+
+con <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  host = "oxfam-project.c2oxbyqo9drv.us-east-1.rds.amazonaws.com",
+  port = 5432,
+  dbname = "oxfam",
+  user = "postgres",
+  password = Sys.getenv("DB_PASSWORD")
+)
+tables <- dbListTables(con)
+
+indicadores <- dplyr::tbl(con, "indicadores") |> dplyr::collect()
+
+available_slug <- unique(indicadores$slug)
+unit_info <-  read_sheet("https://docs.google.com/spreadsheets/d/1tjMuZuPliEdssJjqZtTKsOC8x5WR3ENwlWoCp-Dhhvk/edit#gid=0", "filtros_detalle_unidades")
+unit_translate <- unit_info |>
+  select(slug, unidad, unidad_es, unidad_en, unidade_pt) |>
+  drop_na(unidad)
+unit_translate$unidade_pt <- coalesce(unit_translate$unidade_pt, unit_translate$unidad)
+
+countries_info <- read_sheet("https://docs.google.com/spreadsheets/d/1tjMuZuPliEdssJjqZtTKsOC8x5WR3ENwlWoCp-Dhhvk/edit#gid=0", "variable_pais")
+countries_translate <- countries_info |>
+  select(pais = pais_es, pais_en, pais_pt)
+
+translate_func <- function(df, slug_i) {
+  df <- df |>
+    filter(slug %in% slug_i) |>
+    separate_rows(unidad, sep = "\\|")
+  df <- df |> left_join(unit_translate)
+  df <- df |> left_join(countries_translate)
+  df <- df |> left_join(slug_translate)
+  df$unidad_es <- coalesce(df$unidad_es, df$unidad)
+  df$unidad_es[df$unidad_es == ""] <- NA
+  df$unidad_en <- coalesce(df$unidad_en, df$unidad)
+  df$unidad_en[df$unidad_en == ""] <- NA
+  df
+}
+
+
+slug_spanish <- map(available_slug, function(slug_i) {
+  df <- translate_func(indicadores, slug_i)
+  df_es <- df |> select(id, slug, slug_es, fecha, pais, valor, unidad = unidad_es)
+  df_es <- Filter(function(x) !all(is.na(x)), df_es)
+  df_es
+})
+names(slug_spanish) <- available_slug
+slug_spanish
+
+slug_english <- map(available_slug, function(slug_i) {
+  df <- translate_func(indicadores, slug_i)
+  df_en <- df |> select(id, slug, slug_en, fecha, pais = pais_en, valor, unidad = unidad_en)
+  df_en <- Filter(function(x) !all(is.na(x)), df_en)
+  df_en
+})
+names(slug_english) <- available_slug
+slug_english
+
+
+slug_portuges <- map(available_slug, function(slug_i) {
+  df <- translate_func(indicadores, slug_i)
+  df_pt <- df |> select(id, slug, slug_pt, fecha, pais = pais_pt, valor, unidad = unidade_pt)
+  df_pt <- Filter(function(x) !all(is.na(x)), df_pt)
+  df_pt
+})
+names(slug_portuges) <- available_slug
+slug_portuges
+
+oxfam_data <- list(
+  es = slug_spanish,
+  en = slug_english,
+  pt = slug_portuges
+)
+
+
+usethis::use_data(oxfam_data, overwrite = TRUE)
+#https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/highcharts/demo/combo-multi-axes
